1a6046e62533b360bbaf9c381d13641c59795407
Konstantin Vorobyev <kvorobyev@gmail.com>
2012-12-02 22:09:06 +0900

    Untrusted certificates support

--- android/Lima1SyncService/src/org/kvj/lima1/sync/controller/net/HttpClientTransport.java
+++ android/Lima1SyncService/src/org/kvj/lima1/sync/controller/net/HttpClientTransport.java
@@ -3,9 +3,22 @@ package org.kvj.lima1.sync.controller.net;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
@@ -45,6 +58,43 @@ public class HttpClientTransport implements NetTransport {
 		proxyPort = Proxy.getPort(context);
 	}
 
+	public class MySSLSocketFactory extends SSLSocketFactory {
+		SSLContext sslContext = SSLContext.getInstance("TLS");
+
+		public MySSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException,
+				KeyStoreException, UnrecoverableKeyException {
+			super(truststore);
+
+			TrustManager tm = new X509TrustManager() {
+				@Override
+				public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
+
+				@Override
+				public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
+
+				@Override
+				public X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+			};
+
+			sslContext.init(null, new TrustManager[] { tm }, null);
+		}
+
+		@Override
+		public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException,
+				UnknownHostException {
+			return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+		}
+
+		@Override
+		public Socket createSocket() throws IOException {
+			return sslContext.getSocketFactory().createSocket();
+		}
+	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	public JSONObject request(String uri, RequestType type, Object data, String contentType)
@@ -56,7 +106,12 @@ public class HttpClientTransport implements NetTransport {
 			HttpParams params = httpClient.getParams();
 			SchemeRegistry schemeRegistry = new SchemeRegistry();
 			schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-			schemeRegistry.register(new Scheme("https", SSLSocketFactory.getSocketFactory(), 443));
+			KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+			trustStore.load(null, null);
+
+			SSLSocketFactory sf = new MySSLSocketFactory(trustStore);
+			sf.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+			schemeRegistry.register(new Scheme("https", sf, 443));
 			ThreadSafeClientConnManager cm = new ThreadSafeClientConnManager(params, schemeRegistry);
 			httpClient.setParams(params);
 
